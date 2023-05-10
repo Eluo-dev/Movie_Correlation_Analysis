c19e3405087de00dbb8a6cbe9c21729539172a44
Elwin <132974039+Vetora@users.noreply.github.com>
2023-05-09 22:26:25 -0700

    Hypothesis Update

--- Movie_Correlation_Project.py
+++ Movie_Correlation_Project.py
@@ -2,7 +2,7 @@
 Project task: Develop visuals to display what variables correlate most to movie gross earnings, scrape data from
 csv, show how data correlates using plots and heatmaps, sort and analyze the data and produce conclusion
 
-# Hypothesis: Budget will be highly correlated with gross revenue, analyze the data to verify if this is true
+# Hypothesis: budget and company will be highly correlated with gross revenue, analyze the data to verify if this is true
 
 # Pycharm not displaying all csv data so I used Jupyter Notebook instead of Pycharm
 '''
