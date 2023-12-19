b095dbcf426fe0e523612386ba32fd4a0a1f99ed
Elwin Luo <132974039+Vetora@users.noreply.github.com>
2023-12-19 14:36:43 -0800

    Update Movie_Correlation_Project.py

--- Movie_Correlation_Project.py
+++ Movie_Correlation_Project.py
@@ -1,12 +1,3 @@
-'''
-Project task: Develop visuals to display what variables correlate most to movie gross earnings, scrape data from
-csv, show how data correlates using plots and heatmaps, sort and analyze the data and produce conclusion
-
-# Hypothesis: budget and company will be highly correlated with gross revenue, analyze the data to verify if this is true
-
-# Pycharm not displaying all csv data so I used Jupyter Notebook instead of Pycharm
-'''
-
 # import libraries
 
 import numpy as np
